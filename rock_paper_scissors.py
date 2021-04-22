a205169a92848f081cdf11373eb4cbc98e13de16
g-sivaprasad <sivaprasad.g@live.com>
2021-04-22 11:03:12 +0530

    add elif draw condtion

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -27,27 +27,23 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-u_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
-c_choice = random.randint(0, 2) 
-
 game = [rock, paper, scissors]
-choice =[u_choice, c_choice] 
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+computer_choice = random.randint(0, 2) 
+choice =[user_choice, computer_choice] 
 
-if choice[0] <= 2:
-    if choice[0] == 0 and choice[1] == 2:
-        print(f"{game[0]}\nComputer Choice\n{game[2]}")
-        print("You win")
-    elif choice[0] == 2 and choice[1] == 1:
-            print(f"{game[2]}\nComputer Choice\n{game[1]}")
-            print("You win")
-    elif choice[0] == 1 and choice[1] == 0:
-            print(f"{game[1]}\nComputer Choice\n{game[0]}")
-            print("You win")
-    else:
-        print(f"{game[choice[0]]}\nComputer Choice\n{game[choice[1]]}")
-        print("You lose ") 
+if choice[0] >2  and choice[0] < 0:
+    print("You typed an invalid number, you lose!\n") 
+elif choice[0] == 0 and choice[1] == 2:
+    print(f"{game[0]}\nComputer Chose\n{game[2]}\nYou win\n")
+elif choice[0] == 2 and choice[1] == 1:
+    print(f"{game[2]}\nComputer Chose\n{game[1]}\nYou win\n")
+elif choice[0] == 1 and choice[1] == 0:
+    print(f"{game[1]}\nComputer Chose\n{game[0]}\nYou win\n")
+elif choice[0] == choice[1]:
+    print(f"{game[choice[0]]}\nComputer Chose\n{game[choice[1]]}\nit's a draw\n")
 else:
-    print("wrong number! please try again.")
+    print(f"{game[choice[0]]}\nComputer Chose\n{game[choice[1]]}You lose\n")
 
 
 
